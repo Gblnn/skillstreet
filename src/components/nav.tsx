ad44ee79e80e05669b26f0295dfeae6d25b2dadc
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-01-22 23:01:30 +0400

    update

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -14,7 +14,7 @@ export default function Nav(){
 
             <div style={{marginRight:"2rem"}}>
 
-                {/* <button className='btn'><a href='#access'>Early Access</a></button> */}
+                <button className='btn'><a href='#access'>Early Access</a></button>
 
             </div>
 
